678841e82fd4ab27e4f7e025dfbf016890420205
Naomi S <naomiserkez@gmail.com>
2024-08-15 15:47:13 -0400

    clean up HelpActivity.java

--- app/src/main/java/com/example/dibapp/activities/HelpActivity.java
+++ app/src/main/java/com/example/dibapp/activities/HelpActivity.java
@@ -1,60 +1,40 @@
 package com.example.dibapp.activities;
 
-import android.content.res.Configuration;
 import android.os.Bundle;
-
 import androidx.appcompat.widget.Toolbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.example.dibapp.R;
 import com.example.dibapp.classes.HelpAdapter;
 import com.example.dibapp.classes.Utils;
 import com.example.dibapp.model.HelpItem;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-
 import java.util.ArrayList;
-/*
-
-public class HelpActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_help);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
-}
-*/
-
-import android.os.Bundle;
-
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
-import java.util.ArrayList;
 import java.util.List;
 
 public class HelpActivity extends AppCompatActivity {
+
+    // Help Activity is where all the possible things to text the chat is displayed
+    // It also contains a back button and a FAB for viewing about section (2nd way to view it)
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+        setupToolbar();
+        setupView();
+        setupFAB();
+    }
 
+    private void setupToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
+    private void setupView() {
         RecyclerView recyclerView = findViewById(R.id.rv_help_item);
         final int RV_COLUMN_COUNT = getResources().getInteger(R.integer.rv_help_columns);
         recyclerView.setLayoutManager(new GridLayoutManager(this, RV_COLUMN_COUNT));
@@ -62,6 +42,9 @@ public class HelpActivity extends AppCompatActivity {
         List<HelpItem> helpItemList = getHelpItemList();
         HelpAdapter helpAdapter = new HelpAdapter(helpItemList);
         recyclerView.setAdapter(helpAdapter);
+    }
+
+    private void setupFAB() {
         FloatingActionButton fabAbout = findViewById(R.id.fab_about);
         fabAbout.setOnClickListener(view -> handleFABClick());
     }
